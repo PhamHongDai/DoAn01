58d7e00a0e24e6f1ad8e19f7f0a2dd95c9e8abeb
dactruong <dactruong2306@gmail.com>
2021-12-11 00:06:13 +0700

    aaaaa

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $(".owl-carousel").owlCarousel({
-        items: 1,
-        loop: true,
-        mouseDrag: true,
-        autoplay: true, // cho item chạy
-        autoplayTimeout: 5000, // chỉnh tốc chạy 1000 = 1s
-    });
-});
