254c5b00d76919b5e3c3df2f2b2749a6bbe853ef
dactruong <dactruong2306@gmail.com>
2021-12-12 10:22:52 +0700

    DoAn

--- DoAnCNTT/src/main/java/com/ute/dbms/webshop/controller/AdminController.java
+++ DoAnCNTT/src/main/java/com/ute/dbms/webshop/controller/AdminController.java
@@ -125,90 +125,50 @@ public class AdminController {
         model.addAttribute("orderFormList", orderFormList);
         return "admin-donhang";
     }
-    @GetMapping("/order/detail/{id}")
-    public String detailOrder(@PathVariable("id") int id, HttpServletRequest request, Model model){
-        User user = userRepository.findByEmail(request.getUserPrincipal().getName());
-        UserForm userForm = new UserForm(
-                user.getEmail(),user.getUserInfo().getUserName(),
-                user.getUserInfo().getPhone(),user.getUserInfo().getAddress());
-        userForm.setId(id);
-        Bill bill = billRepository.findById(id);
-        List<BillForm> billFormList = new ArrayList<BillForm>();
-        List<Detail> detailList = detailRepository.findAllById(id);
-        for(Detail detail : detailList){
-            BillForm billForm = new BillForm();
-            billForm.setNamePro(detail.getProduct().getName());
-            billForm.setPrice(detail.getProduct().getPrice());
-            billForm.setQuantily(detail.getQuantily());
-            billForm.setSum(detail.getProduct().getPrice(), detail.getQuantily());
-            billFormList.add(billForm);
-        }
-        System.out.println(id);
-        for(BillForm billForm : billFormList)
-            System.out.println(billForm.toString());
-        model.addAttribute("billFormList", billFormList);
-        model.addAttribute("bill",bill);
-        model.addAttribute("sumBill", bill.getSum());
-        model.addAttribute("userForm", userForm);
-
-        return "admin-detail";
-    }
-    @GetMapping("/order/detail/delivery/{id}")
-    public String deliveryOrder(@PathVariable("id") int id){
-        Bill bill = billRepository.findById(id);
-        bill.setStatus(true);
-        billRepository.save(bill);
-        return "redirect:/admin/order";
-    }
-    @GetMapping("/sales")
-    public String sales(Model model){
-        List<Bill> billList = billRepository.findAllByStatus(true);
-        int sum = 0;
-        for(Bill bill : billList)
-            sum += bill.getSum();
-        model.addAttribute("billList", billList);
-        model.addAttribute("sum",sum);
-        return "admin-doanhthu";
-    }
-//    @GetMapping("/nhanvien")
-//    public String nhanvien(Model model){
-//        List<User> list = userRepository.findAll();
-//        for (User user : list) {
-//            for (Role role : user.getRoles()) {
-//                System.out.println(role);
-//                if(role.equals("ROLE_STAFF"))
-//                    list.remove(user);
-//            }
+//    @GetMapping("/order/detail/{id}")
+//    public String detailOrder(@PathVariable("id") int id, HttpServletRequest request, Model model){
+//        User user = userRepository.findByEmail(request.getUserPrincipal().getName());
+//        UserForm userForm = new UserForm(
+//                user.getEmail(),user.getUserInfo().getUserName(),
+//                user.getUserInfo().getPhone(),user.getUserInfo().getAddress());
+//        userForm.setId(id);
+//        Bill bill = billRepository.findById(id);
+//        List<BillForm> billFormList = new ArrayList<BillForm>();
+//        List<Detail> detailList = detailRepository.findAllById(id);
+//        for(Detail detail : detailList){
+//            BillForm billForm = new BillForm();
+//            billForm.setNamePro(detail.getProduct().getName());
+//            billForm.setPrice(detail.getProduct().getPrice());
+//            billForm.setQuantily(detail.getQuantily());
+//            billForm.setSum(detail.getProduct().getPrice(), detail.getQuantily());
+//            billFormList.add(billForm);
 //        }
-//        model.addAttribute("accounts", list);
+//        System.out.println(id);
+//        for(BillForm billForm : billFormList)
+//            System.out.println(billForm.toString());
+//        model.addAttribute("billFormList", billFormList);
+//        model.addAttribute("bill",bill);
+//        model.addAttribute("sumBill", bill.getSum());
+//        model.addAttribute("userForm", userForm);
 //
-//        return "/admin-nv";
+//        return "admin-detail";
 //    }
-//    @GetMapping("/nhanvien/add")
-//    public String ThemNhanVien(HttpServletRequest request, Model model){
-//        if(request.isUserInRole("ADMIN")){
-//            model.addAttribute("userForm", new UserForm());
-//            return "/add-nv";
-//        }
-//        return "/admin-nv";
+//    @GetMapping("/order/detail/delivery/{id}")
+//    public String deliveryOrder(@PathVariable("id") int id){
+//        Bill bill = billRepository.findById(id);
+//        bill.setStatus(true);
+//        billRepository.save(bill);
+//        return "redirect:/admin/order";
 //    }
-//    @PostMapping("/nhanvien/register")
-//    public String register(@Valid UserForm userForm){
-//        System.out.println(userForm.getEmail());
-//        if(userRepository.findByEmail(userForm.getEmail()) == null){
-//            User oth = new User();
-//            oth.setEmail(userForm.getEmail());
-//            oth.setPassword(passwordEncoder.encode(userForm.getPassword()));
-//            userRepository.save(oth);
-//            UserInfo userInfo = new UserInfo(userForm.getUserName(), userForm.getPhone(), userForm.getAddress());
-//            oth.setUserInfo(userInfo);
-//            userInfo.setUser(oth);
-//            HashSet<Role> roles = new HashSet<>();
-//            roles.add(roleRepository.findByRoleName("ROLE_STAFF"));
-//            oth.setRoles(roles);
-//            userRepository.save(oth);
-//            return "redirect:/admin-nv";
-//        }
-//        return "redirect:/signup?error";
+//    @GetMapping("/sales")
+//    public String sales(Model model){
+//        List<Bill> billList = billRepository.findAllByStatus(true);
+//        int sum = 0;
+//        for(Bill bill : billList)
+//            sum += bill.getSum();
+//        model.addAttribute("billList", billList);
+//        model.addAttribute("sum",sum);
+//        return "admin-doanhthu";
 //    }
+
 }
